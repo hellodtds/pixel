c259f04637e918d9a40abb94a354e4b68d61e408
hellodtds <hello@scottedmonds.com>
2018-03-15 16:39:59 -0400

    App.js clean-up of comments

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-
-
-/***** IMPORTANT TODO: Consolidate local and copeden work *****/
-
-                         /* TODO: Push all work to GitHub */
-
-
 /* VARIABLE ASSIGNMENT */
 
 var $submit = $("input[type=submit]");
@@ -14,7 +7,6 @@ var inputWidth = $("#inputWidth").attr("value", 10);
 
 
 
-/* FUNCTION: MAKE GRID! */
 
 /** DONE: add input value functionality **/
 
@@ -23,7 +15,7 @@ $submit.on("click", function(evt){
     makeGrid(inputHeight.val(), inputWidth.val());
 });
 
-/* FUNCTION: Make a grid! */
+/* FUNCTION: MAKE GRID! */
 function makeGrid(height, width) {
 
     for (var row = 0; row < height; row++) {
@@ -42,33 +34,6 @@ function makeGrid(height, width) {
 }
 
 
-/***** IMPORTANT TODO: Consolidate local and copeden work *****/
-
-                         /* TODO: Push all work to GitHub */
-
-
-
-/* FUNCTION: Pick a color! */
-
-/** TODO: Build colorPicker functionality
-1) Select Color
-2) Assign Selected Color to currentColor
-3) Test: console.log(current color)
-
-Fixed: Able to set color for table, not for td. Why?
-REFERENCE:
-  http://api.jquery.com/on/
-  => ".on( events [, selector ] [, data ], handler )"
-
-**/
-
-/** TODO: Create function for colorPicker
-TODO: Fix for "submit" appending additonal grids
-TODO: V2: Feature to reset grid
-TODO: V2: Mobile-compliance for colorPicker
-TODO: V2: Feature for removing color
-
-**/
 
 
 $('#pixelCanvas').on('click', 'td', function() {
@@ -77,13 +42,4 @@ $('#pixelCanvas').on('click', 'td', function() {
     $(this).css("backgroundColor",currentColor);
 
 });
-
-
-/***** IMPORTANT TODO: Consolidate local and copeden work *****/
-
-                         /* TODO: Push all work to GitHub */
-
-
-
-
 
