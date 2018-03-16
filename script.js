918ecc8241638abaeb071353c24055ee5c8512e5
hellodtds <hello@scottedmonds.com>
2018-03-16 19:08:50 -0400

    fix: reset of grid breaks colorPicker functionality (todo)

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ var inputWidth = $("#inputWidth").attr("value", 10);
 var colorPicker = $("input[type=color]");
 var currentColor = colorPicker.val();
 
+// todo: better placement
 const reset = document.createElement("button");
-
 reset.textContent = "Reset Grid";
 reset.setAttribute('id', 'reset');
 
+
+/** Methods **/
+
 function makeGrid(height, width) {
-
-
     for (let row = 0; row < height; row++) {
         let table = document.querySelector("#pixelCanvas");
         let tr = document.createElement("tr");
@@ -24,15 +25,10 @@ function makeGrid(height, width) {
         }
         table.appendChild(tr);
         table.after(reset);
-
     }
-
 }
 
-reset.addEventListener('click', resetGrid, false);
-
 function addColor() {
-
     $(this).css("backgroundColor",currentColor);
 
     if (currentColor) {
@@ -40,21 +36,27 @@ function addColor() {
     }
 }
 
-/** Event: Submit Grid Row and Column Length **/
+function resetGrid() {
+        reset.remove();
+        $('#pixelCanvas').find('tr').remove();
+        makeGrid(inputHeight.val(), inputWidth.val());
+}
+
+
+
+/** Event Listeners **/
+
+// Creating Grid
 $submit.on("click", function(evt){
     evt.preventDefault();
     makeGrid(inputHeight.val(), inputWidth.val());
 });
 
+// Adding color
 $('#pixelCanvas').on('click', 'td', addColor);
 
-
-function resetGrid() {
-        reset.remove();
-        $('#pixelCanvas').find('tr').remove();
-}
-
-
+// Resetting grid
+reset.addEventListener('click', resetGrid, false);
 
 
 
