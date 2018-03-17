f84c816a69e2dfc9edbd3dac29890035cb4e19c4
hellodtds <hello@scottedmonds.com>
2018-03-16 20:53:38 -0400

    to fix: removal of documentFragment when submit button pressed and grid present

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 /** Variables **/
 
-var $submit = $("input[type=submit]");
+var submit = $("input[type=submit]");
 var inputHeight = $("#inputHeight").attr("value", 10); // inital height, width = 10
 var inputWidth = $("#inputWidth").attr("value", 10);
 
 var colorPicker = $("input[type=color]");
 var currentColor = colorPicker.val();
+
+let table = document.querySelector('table');
+
 
 // todo: better placement
 const reset = document.createElement("button");
@@ -26,34 +29,45 @@ function makeGrid(height, width) {
         table.appendChild(tr);
         table.after(reset);
     }
+
 }
 
-function addColor() {
-    $(this).css("backgroundColor",currentColor);
+function addColor(evt) {
+    let defaultColor = document.querySelector("input[type=color]").value;
+    evt.target.style.backgroundColor = defaultColor;
 
-    if (currentColor) {
-        console.log("the color is " + currentColor);
-    }
 }
 
 function resetGrid() {
         reset.remove();
         $('#pixelCanvas').find('tr').remove();
         makeGrid(inputHeight.val(), inputWidth.val());
+        document.querySelector('td').addEventListener('click', addColor);
 }
-
 
 
 /** Event Listeners **/
 
 // Creating Grid
-$submit.on("click", function(evt){
+submit.on("click", function(evt){
     evt.preventDefault();
-    makeGrid(inputHeight.val(), inputWidth.val());
+    let designCanvas = document.querySelector('.module').previousElementSibling;
+    let enforcer = document.createDocumentFragment();
+
+    if (enforcer.textContent === "Action Unavailable") {
+        enforcer.textContent = "";
+        submit.disabled = "true";
+    }
+    else if (table.hasChildNodes() === true) {
+        enforcer.textContent = "Action Unavailable";
+        designCanvas.before(enforcer);
+    } else {
+        makeGrid(inputHeight.val(), inputWidth.val());
+    }
 });
 
 // Adding color
-$('#pixelCanvas').on('click', 'td', addColor);
+$('table').on('click', "td", addColor);
 
 // Resetting grid
 reset.addEventListener('click', resetGrid, false);
